a01d46c2e3b53e6ce890c7fcf0bd8c8f31ddba3b
elmohawk <mrhawk2007@gmail.com>
2026-07-02 17:23:45 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
+const slides = document.querySelectorAll(".hero-slide");
+const dots = document.querySelectorAll(".dot");
 
+let current = 0;
+
+function showSlide(index){
+
+    slides.forEach(slide=>{
+        slide.classList.remove("active");
+    });
+
+    dots.forEach(dot=>{
+        dot.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+
+    dots[index].classList.add("active");
+
+}
+
+function nextSlide(){
+
+    current++;
+
+    if(current >= slides.length){
+
+        current = 0;
+
+    }
+
+    showSlide(current);
+
+}
+
+setInterval(nextSlide,6000);
