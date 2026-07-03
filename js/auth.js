a8cecb31038f07ac3ca37467412833eb89dc910c
elmohawk <mrhawk2007@gmail.com>
2026-07-03 11:41:54 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -3,66 +3,133 @@
 // ===========================================
 
 // SIGN UP
-async function signUp(email, password){
+async function register(email, password, username){
 
     const { data, error } = await supabase.auth.signUp({
+
         email,
-        password
+        password,
+
+        options:{
+            data:{
+                username
+            }
+        }
+
     });
 
     if(error){
+
         alert(error.message);
+
         return;
+
     }
 
-    alert("Account created successfully!");
-    window.location.href = "login.html";
+    alert("Account created successfully.");
 
 }
-
-
 // LOGIN
-async function signIn(email, password){
+async function login(email,password){
 
-    const { data, error } = await supabase.auth.signInWithPassword({
+    const { error } = await supabase.auth.signInWithPassword({
+
         email,
         password
+
     });
 
     if(error){
+
         alert(error.message);
+
         return;
+
     }
 
-    alert("Login successful!");
-    window.location.href = "index.html";
+    window.location="index.html";
 
 }
-
-
 // LOGOUT
 async function logout(){
 
     await supabase.auth.signOut();
 
-    window.location.href = "login.html";
+    window.location="login.html";
 
 }
-
 
 // GET CURRENT USER
-function getUser(){
+async function getCurrentUser(){
 
-    return supabase.auth.getUser();
+    const {
+
+        data:{user}
+
+    } = await supabase.auth.getUser();
+
+    return user;
 
 }
-
-
 // CHECK SESSION ON LOAD
-async function checkAuth(){
+async function requireLogin(){
 
-    const { data } = await supabase.auth.getSession();
+    const user = await getCurrentUser();
 
-    return data.session;
+    if(!user){
+
+        window.location="login.html";
+
+    }
 
 }
+async function requireAdmin(){
+
+    const user = await getCurrentUser();
+
+    if(!user){
+
+        location = "login.html";
+
+        return;
+
+    }
+
+    const { data } = await supabase
+        .from("profiles")
+        .select("role")
+        .eq("id", user.id)
+        .single();
+
+    if(data.role !== "admin"){
+
+        location = "index.html";
+
+    }
+
+}
+async function updateNavbar(){
+
+    const user = await getCurrentUser();
+
+    const loginBtn = document.querySelector(".login-btn");
+
+    if(!loginBtn) return;
+
+    if(user){
+
+        loginBtn.innerHTML = "My Account";
+
+        loginBtn.href = "profile.html";
+
+    }else{
+
+        loginBtn.innerHTML = "Login";
+
+        loginBtn.href = "login.html";
+
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", updateNavbar);
