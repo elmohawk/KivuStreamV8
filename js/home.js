08e528b7fd9bbecaeb922b623d988e5d96698a65
elmohawk <mrhawk2007@gmail.com>
2026-07-04 11:45:14 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,14 +1,58 @@
 document.addEventListener("DOMContentLoaded", async () => {
 
-    const trending = await API.getTrendingMovies();
+    await loadTrending();
 
-    if (!trending) return;
+    await loadPopular();
 
-    const container = document.getElementById("trendingMovies");
+    await loadTopRated();
 
-    container.innerHTML = trending.results
+    await loadSeries();
+
+});
+
+async function loadTrending() {
+
+    const data = await getTrendingMovies();
+
+    renderMovies("trendingMovies", data);
+
+}
+
+async function loadPopular() {
+
+    const data = await getPopularMovies();
+
+    renderMovies("popularMovies", data);
+
+}
+
+async function loadTopRated() {
+
+    const data = await getTopRatedMovies();
+
+    renderMovies("topRatedMovies", data);
+
+}
+
+async function loadSeries() {
+
+    const data = await getPopularSeries();
+
+    renderMovies("popularSeries", data);
+
+}
+
+function renderMovies(id, data) {
+
+    if (!data) return;
+
+    const container = document.getElementById(id);
+
+    if (!container) return;
+
+    container.innerHTML = data.results
         .slice(0, 12)
         .map(createMovieCard)
         .join("");
 
-});
+}
