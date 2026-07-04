aefa39e201774f754c4affc17d18f78ae347044d
elmohawk <mrhawk2007@gmail.com>
2026-07-04 11:44:48 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,21 +1,38 @@
 function createMovieCard(movie) {
+
+    const poster = movie.poster_path
+        ? `${CONFIG.TMDB_IMAGE_URL}${movie.poster_path}`
+        : "assets/images/no-image.png";
+
     return `
-        <article class="movie-card">
+
+        <div class="movie-card">
+
             <a href="watch.html?id=${movie.id}">
+
                 <img
-                    src="${CONFIG.TMDB_IMAGE_URL}${movie.poster_path}"
+                    src="${poster}"
                     alt="${movie.title}"
                     loading="lazy"
                 >
 
                 <div class="movie-info">
+
                     <h3>${movie.title}</h3>
 
-                    <p>⭐ ${movie.vote_average.toFixed(1)}</p>
+                    <p class="rating">
+                        ⭐ ${movie.vote_average.toFixed(1)}
+                    </p>
 
-                    <small>${movie.release_date || "Unknown"}</small>
+                    <small>
+                        ${movie.release_date || "Coming Soon"}
+                    </small>
+
                 </div>
+
             </a>
-        </article>
+
+        </div>
+
     `;
 }
