83d49429019e00890375124373b1e0c2359f0702
elmohawk <mrhawk2007@gmail.com>
2026-07-04 12:05:59 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,3 +10,45 @@ document.addEventListener("DOMContentLoaded", async () => {
     await loadComponent("footer", "components/footer.html");
 
 });
+async function updateNavbar() {
+
+    const authMenu = document.getElementById("authMenu");
+
+    if (!authMenu) return;
+
+    const user = await getCurrentUser();
+
+    if (user) {
+
+        authMenu.innerHTML = `
+            <a href="profile.html" class="login-btn">
+                My Profile
+            </a>
+
+            <button
+                id="logoutBtn"
+                class="login-btn">
+                Logout
+            </button>
+        `;
+
+        document
+            .getElementById("logoutBtn")
+            .addEventListener("click", logout);
+
+    } else {
+
+        authMenu.innerHTML = `
+            <a href="login.html" class="login-btn">
+                Login
+            </a>
+
+            <a href="register.html"
+                class="btn btn-secondary">
+                Register
+            </a>
+        `;
+
+    }
+
+}
