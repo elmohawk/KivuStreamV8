7c725a6abaea6c1b9aacbc43d1c4b0f2f581ffd1
elmohawk <mrhawk2007@gmail.com>
2026-07-04 11:53:57 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,12 +1,4 @@
-// =====================================
-// KIVUSTREAM SUPABASE
-// =====================================
-
-const SUPABASE_URL = "https://exjgejujfxejjlbfizgz.supabase.co";
-
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4amdlanVqZnhlampsYmZpemd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MTQzMTQsImV4cCI6MjA5NDA5MDMxNH0.CWUYLk4qJfriIYXWScB7wcHHVTCuz0SGDhWUV3tMR1Y";
-
 const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
+    CONFIG.SUPABASE_URL,
+    CONFIG.SUPABASE_ANON_KEY
 );
