8de50072f7f6e8c8da5cffe660e1bd294dc68fa7
elmohawk <mrhawk2007@gmail.com>
2026-07-04 11:39:09 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
+async function loadComponent(id, file) {
+    const response = await fetch(file);
+    const html = await response.text();
+    document.getElementById(id).innerHTML = html;
+}
 
+document.addEventListener("DOMContentLoaded", async () => {
+
+    await loadComponent("navbar", "components/navbar.html");
+    await loadComponent("footer", "components/footer.html");
+
+});
