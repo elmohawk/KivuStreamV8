ff2a6f8c19eaaf716965584028ca385d0723a273
elmohawk <mrhawk2007@gmail.com>
2026-07-04 11:53:00 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,13 @@
 const CONFIG = {
-    TMDB_API_KEY: "YOUR_TMDB_API_KEY",
-    TMDB_BASE_URL: "https://api.themoviedb.org/3",
-    TMDB_IMAGE_URL: "https://image.tmdb.org/t/p/w500"
+
+    TMDB_API_KEY:"YOUR_TMDB_KEY",
+
+    TMDB_BASE_URL:"https://api.themoviedb.org/3",
+
+    TMDB_IMAGE_URL:"https://image.tmdb.org/t/p/w500",
+
+    SUPABASE_URL:"YOUR_SUPABASE_URL",
+
+    SUPABASE_ANON_KEY:"YOUR_SUPABASE_ANON_KEY"
+
 };
